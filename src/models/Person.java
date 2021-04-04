30c0b64da5bacef14f8c245806f7cbe976c23c4b
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-04-03 20:44:46 -0400

    created  model class person with valid tests.

---
+++ src/models/Person.java
@@ -0,0 +1,97 @@
+package models;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public abstract class Person {
+    private String firstName, lastName;
+    private int strength;
+    private LocalDate birthday;
+    private ArrayList<String> specialMoves;
+
+// constructor with five parameters.
+    public Person(String firstName, String lastName, int strength, LocalDate birthday, ArrayList<String> specialMoves) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStrength(strength);
+        setBirthday(birthday);
+        setSpecialMoves(specialMoves);
+    }
+    /*
+        abstract method
+     */
+    abstract int getRank();
+    //setter and getter methods with validations.
+    public void setFirstName(String firstName) {
+        if (firstName.matches("[A-Z][a-z]*")) {
+            this.firstName = firstName;
+        }
+        else {
+            throw new IllegalArgumentException("Start the FirstName with capital Letters and Dont Use Numeric Values or characters");
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.matches("[A-Z][a-z]*")) {
+            this.lastName = lastName;
+        }
+        else {
+            throw new IllegalArgumentException("Start the LastName with capital Letters and Dont Use Numeric Values or characters");
+        }
+    }
+
+    public void setStrength(int strength) {
+        if (strength<=100 && strength>=0) {
+            this.strength = strength;
+        }
+        else{
+            throw new IllegalArgumentException("Strength should be marked between 0 & 100"+ strength + " is not valid");
+        }
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        if (birthday.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Invalid birthday, Are U from future!!");
+        }
+        else{
+            this.birthday = birthday;
+        }
+    }
+
+    public void setSpecialMoves(ArrayList<String> specialMoves) {
+        if (specialMoves.size() >= 2) {
+            this.specialMoves = specialMoves;
+        }
+        else{
+            throw new IllegalArgumentException("You Must define at least two special moves thats the eligibility criteria");
+        }
+    }
+    /*
+    this functions return the current age og the person.
+     */
+    public int getAge(){
+        return Period.between(birthday, LocalDate.now()).getYears();
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public ArrayList<String> getSpecialMoves() {
+        return specialMoves;
+    }
+}
---
+++ src/tests/models/PersonTest.java
@@ -0,0 +1,131 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    Person person;
+
+    @BeforeEach
+    void setUp() {
+        ArrayList<String> moves = new ArrayList<>();
+        moves.add("stealth moves");
+        moves.add("element of surprise");
+        person = new Person("Akshit", "Deswal", 90, LocalDate.of(2001, 8, 13), moves) {
+            @Override
+            int getRank() {
+                return 0;
+            }
+        };
+        person.setSpecialMoves(moves);
+    }
+
+
+    @Test
+    void setFirstName() {
+        person.setFirstName("Tanu");
+        assertEquals("Tanu",person.getFirstName());
+    }
+
+    @Test
+    void setFirstNameInvalidNumericEntry(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setFirstName("AKM47"));
+    }
+
+    @Test
+    void setFirstNameInvalidEmpty(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setFirstName(""));
+    }
+
+    @Test
+    void setFirstNameInvalidLessLetters(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setFirstName("AD"));
+    }
+
+    @Test
+    void setLastName() {
+        person.setLastName("Dhindsa");
+        assertEquals("Dhindsa",person.getLastName());
+    }
+
+    @Test
+    void setLastNameInvalidNumericEntry(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setLastName("AKM47"));
+    }
+
+    @Test
+    void setLastNameInvalidEmpty(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setLastName(""));
+    }
+
+    @Test
+    void setLastNameInvalidLessLetters(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setLastName("AD"));
+    }
+
+    @Test
+    void setStrength() {
+        person.setStrength(95);
+        assertEquals(95, person.getStrength());
+    }
+
+    @Test
+    void setStrengthIvalidLess(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setStrength(-20));
+
+    }
+
+    @Test
+    void setStrengthIvalidHigh(){
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setStrength(120));
+
+    }
+
+    @Test
+    void setBirthday() {
+        person.setBirthday(LocalDate.of(2001,9,13));
+        assertEquals(LocalDate.of(2001,9,13), person.getBirthday());
+    }
+
+    @Test
+    void setInvalidBirthdayFuture() {
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setBirthday(LocalDate.of(2050,01,01)));
+    }
+
+    @Test
+    void setSpecialMoves() {
+        ArrayList<String> moves = new ArrayList<>();
+        moves.add("stealth move");
+        moves.add("element of surprise");
+        person.setSpecialMoves(moves);
+        assertEquals(moves, person.getSpecialMoves());
+    }
+
+    @Test
+    void setSpecialMovesInvalid() {
+        ArrayList<String> specialPower = new ArrayList<>();
+        specialPower.add("stealth move");
+        assertThrows(IllegalArgumentException.class, ()->
+                person.setSpecialMoves(specialPower));
+    }
+
+    @Test
+    void getAge() {
+        assertEquals(19,person.getAge());
+    }
+
+}
