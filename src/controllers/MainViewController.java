904f4e752c0b3dbb66ed410e6ec34df155e9b854
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-04-15 16:58:00 -0400

    created new view file to create a new hero.

--- src/controllers/MainViewController.java
+++ src/controllers/MainViewController.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 import models.Hero;
 import models.Villain;
 import utilities.DBUtility;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -37,6 +45,18 @@ public class MainViewController implements Initializable {
         }
 
     }
+    @FXML
+    private void createNewHeroButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../views/createHero.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("Create a New Warrior");
+        stage.show();
+    }
+
 
 
 }
